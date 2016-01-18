1055e71c14fa1d94db97b1582b3bc4c89813a48c
Jacob Shay <Jacob Shay>
2016-01-17 19:19:26 -0500

    retrieving store information works

---
+++ cheapSharkAPI/CheapShark.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace cheapSharkAPI
+{
+    public class CheapShark
+    {
+        static string baseUrl = "http://www.cheapshark.com/api/1.0/";
+
+        public CheapShark()
+        {
+
+        }
+
+        /*
+        *
+        *Get list of stores from cheap shark
+        */
+        public string getStores()
+        {
+            string url = baseUrl + "stores";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream resStream = response.GetResponseStream();
+
+            ReadStream readStream = new ReadStream();
+
+            StringBuilder sb = readStream.returnResults(resStream);
+
+            string result = sb.ToString();
+
+            return result;
+        }
+
+
+    }
+}
---
+++ cheapSharkAPI/ReadStream.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+
+namespace cheapSharkAPI
+{
+    class ReadStream
+    {
+        public ReadStream()
+        {
+
+        }
+
+
+        public StringBuilder returnResults(Stream stream)
+        {
+            StreamReader objectReader = new StreamReader(stream);
+
+            StringBuilder results = new StringBuilder();
+
+            while(objectReader.Peek() >= 0)
+            {
+                results.AppendLine(objectReader.ReadLine());
+            }
+            return results;
+        }
+
+    }
+}
